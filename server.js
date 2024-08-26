77d4c6e75de095ccc11efb72138b35552ae1acba
Rajneesh100 <rajneesh_2001cs53@iitp.ac.in>
2024-08-26 15:15:01 +0530

    adding .env file

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express() 
 const db= require('./db')
-
+require('dotenv').config()
+const PORT =process.env.PORT ||3000
 
 app.use(express.json()); //same as body parser
 // const bodyParser =require('body-parser')
@@ -20,4 +21,4 @@ const personRoutes =require('./routes/personRoutes');
 const menuRoutes =require('./routes/menuRoutes');
 app.use('/menu',menuRoutes);
 app.use('/person',personRoutes);
-app.listen(3000,()=>{console.log("Server listing on port 3000")})
+app.listen(PORT,()=>{console.log("Server listing on port 3000")})
